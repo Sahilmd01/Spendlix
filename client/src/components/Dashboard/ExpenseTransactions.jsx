d984b8bb3b0f68bffcd2618e4fbc53fbe114ad90
md sahil <codewithkinu@gmail.com>
2025-07-04 01:00:17 +0530

    transectin and overview set

--- client/src/components/Dashboard/ExpenseTransactions.jsx
+++ client/src/components/Dashboard/ExpenseTransactions.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { LuArrowRight } from "react-icons/lu";
+import moment from "moment";
 
-const ExpenseTransactions = () => {
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
+
+const ExpenseTransactions = ({ transactions, onSeeMore }) => {
+  //console.log("Expense Transactions Data:", transactions);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">Expenses</h5>
 
-export default ExpenseTransactions
+        <button className="card-btn" onClick={onSeeMore}>
+          See All <LuArrowRight className="text-base" />
+        </button>
+      </div>
+
+      <div className="mt-6">
+        {transactions?.slice(0, 5)?.map((expense) => (
+          <TransactionInfoCard
+            key={expense._id}
+            title={expense.category}
+            icon={expense.icon}
+            date={moment(expense.date).format("DD MM YYYY")}
+            amount={expense.amount}
+            type="expense"
+            hideDeleteBtn
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseTransactions;
