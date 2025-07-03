9acd83ae0b66cb4ec675d3165bb85e6f47068270
md sahil <codewithkinu@gmail.com>
2025-07-04 01:09:03 +0530

    expense folder setup and data nevigation

--- client/src/components/Expense/ExpenseList.jsx
+++ client/src/components/Expense/ExpenseList.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { LuDownload } from "react-icons/lu";
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
+import moment from "moment";
 
-const ExpenseList = () => {
+const ExpenseList = ({ transactions, onDelete, onDownload }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">All Expenses</h5>
 
-export default ExpenseList
+        <button className="card-btn" onClick={onDownload}>
+          <LuDownload className="text-base" /> Download
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {transactions?.map((expense) => (
+          <TransactionInfoCard
+            key={expense._id}
+            title={expense.category}
+            icon={expense.icon}
+            date={moment(expense.date).format("DD MM YYYY")}
+            amount={expense.amount}
+            type="expense"
+            onDelete={() => onDelete(expense._id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseList;
--- client/src/components/Expense/ExpenseOverview.jsx
+++ client/src/components/Expense/ExpenseOverview.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { LuPlus } from "react-icons/lu";
 
-const ExpenseOverview = () => {
+import { prepareExpenseLineChartData } from "../../utils/helper";
+import CustomLineChart from "../Charts/CustomLineChart";
+
+const ExpenseOverview = ({ transactions, onExpenseIncome }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const result = prepareExpenseLineChartData(transactions);
+    setChartData(result);
+
+    return () => {};
+  }, [transactions]);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <h5 className="text-lg">Expense Overview</h5>
+          <p className="text-xs text-gray-400 mt-0.5">
+            Track your spending trends over time and gain insights into where
+            your money goes.
+          </p>
+        </div>
+        <button className="add-btn" onClick={onExpenseIncome}>
+          <LuPlus className="text-lg" /> Add Expense
+        </button>
+      </div>
 
-export default ExpenseOverview
+      <div className="mt-10">
+        <CustomLineChart data={chartData} />
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseOverview;
