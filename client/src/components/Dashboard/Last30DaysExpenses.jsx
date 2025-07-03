3504b15b1fe688b46d997c7a562368fb3f24042f
md sahil <codewithkinu@gmail.com>
2025-07-04 01:02:00 +0530

    expense and income tracer set

--- client/src/components/Dashboard/Last30DaysExpenses.jsx
+++ client/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Last30DaysExpenses = () => {
+import { prepareExpenseBarChartData } from "../../utils/helper";
+import CustomBarChart from "../Charts/CustomBarChart";
+
+const Last30DaysExpenses = ({ data }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const result = prepareExpenseBarChartData(data);
+    setChartData(result);
+
+    return () => {};
+  }, [data]);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="card col-span-1">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">Last 30 Days Expenses</h5>
+      </div>
 
-export default Last30DaysExpenses
+      <CustomBarChart data={chartData} />
+    </div>
+  );
+};
+
+export default Last30DaysExpenses;
