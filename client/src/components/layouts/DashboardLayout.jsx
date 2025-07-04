40386c1d1ba1d872d0fe26d47c9cfe1052d1f7d1
md sahil <codewithkinu@gmail.com>
2025-07-04 18:39:36 +0530

    auth and external feature implimented

--- client/src/components/layouts/DashboardLayout.jsx
+++ client/src/components/layouts/DashboardLayout.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
 
-const DashboardLayout = () => {
+import { UserContext } from "../../context/userContext";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
+
+const DashboardLayout = ({ children, activeMenu }) => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="">
+      <Navbar activeMenu={activeMenu} />
+      {user && (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            <SideMenu activeMenu={activeMenu} />
+          </div>
 
-export default DashboardLayout
+          <div className="grow mx-5">{children}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DashboardLayout;
