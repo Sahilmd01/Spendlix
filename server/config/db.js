94d3864f397ed313081915c67ab6be469b7a172d
MD. SAHIL <codewithkinu@gmail.com>
2025-07-07 15:04:28 +0530

    database code modified

--- server/config/db.js
+++ server/config/db.js
@@ -1,25 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-//     console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
-//   } catch (err) {
-//     console.error("❌ MongoDB connection error:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
   } catch (err) {
-    console.error("Error connecting to MongoDB", err);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
