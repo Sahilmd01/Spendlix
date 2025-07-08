40729642dc1707fa5204280baac2cf04854fe6a9
MD. SAHIL <codewithkinu@gmail.com>
2025-07-08 21:23:07 +0530

    database

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB connected ✅ Host: ${conn.connection.host}`);
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error("MongoDB connection error❌ :", err.message);
     process.exit(1);
   }
 };
